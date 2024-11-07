764fc4e47a63c8bc40b30a2cb36e7123d664eecc
Ramon Pêgo <ramon.pego.stl@gmail.com>
2024-11-06 22:05:10 -0300

    chore: code refactor, abort ideia from downloading and storing files, using gzopen insted, better perfomance and less memory usage, next idea use file hash and insert data in batches for less database calls

--- app/Console/Commands/CheckDataFiles.php
+++ app/Console/Commands/CheckDataFiles.php
@@ -10,29 +10,18 @@ use Illuminate\Support\Str;
 
 class CheckDataFiles extends Command
 {
-    protected array $files = [];
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
     protected $signature = 'app:check-data-files {--limit=100}';
-
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
     protected $description = 'Download, extract, and import a limited number of JSON items into the database';
 
-    /**
-     * Execute the console command.
-     */
-    public function handle()
-    {
-        $this->info("Get file names to import...");
-        $this->getFilesToImport();
+    protected array $files = [];
 
+    /**
+     * @return void
+     */
+    public function handle(): void
+    {
+        $this->info("Fetching files to import...");
+        $this->getFilesToImport();
     }
 
     /**
@@ -40,49 +29,57 @@ class CheckDataFiles extends Command
      */
     protected function getFilesToImport(): void
     {
-        $this->files = array_filter(explode("\n", file_get_contents('https://challenges.coode.sh/food/data/json/index.txt')), function ($file) {
-            return Str::endsWith($file, '.gz');
-        });
-        $this->info("Downloading and extracting files...");
-        $this->downloadFiles();
-        $this->cleanDirectories();
+        $this->files = array_filter(
+            explode("\n", file_get_contents('https://challenges.coode.sh/food/data/json/index.txt')),
+            fn($file) => Str::endsWith($file, '.gz')
+        );
+
+        foreach ($this->files as $file) {
+            if ($this->checkIfFileIsImported($file)) continue;
+            $this->downloadAndProcessFile($file);
+            $this->markFileAsImported($file);
+        }
     }
 
     /**
+     * @param string $file
      * @return void
      */
-    protected function downloadFiles(): void
+    protected function downloadAndProcessFile(string $file): void
     {
-        $this->verifyFolders();
-        foreach ($this->files as $file){
-            if ($this->checkIfFileIsImported($file)) {
-                continue;
-            }
-            $url = 'https://challenges.coode.sh/food/data/json/' . $file;
-            $gzFilePath = Storage::path('compressed/'.$file);
-            $jsonFileName = Str::before($file, '.gz');
-            $jsonFilePath = Storage::path('json/'.$jsonFileName);
-            $gzFile = fopen($gzFilePath, 'w');
-            $jsonFile = fopen($jsonFilePath, 'w');
+        $url = 'https://challenges.coode.sh/food/data/json/' . $file;
+        $limit = (int) $this->option('limit');
+        $count = 0;
 
-            $this->info("Downloading {$file}...");
-            $gzStream = fopen($url, 'r');
-            stream_copy_to_stream($gzStream, $gzFile);
-            fclose($gzStream);
-            fclose($gzFile);
-            $this->info("Extracting and saving {$file} into JSON file...");
-            $gzFile = gzopen($gzFilePath, 'rb');
-            while (!gzeof($gzFile)) {
-                fwrite($jsonFile, gzread($gzFile, 4096));
+        $this->info("Downloading and processing {$file}...");
+
+        $gzStream = gzopen($url, 'rb');
+        $buffer = '';
+        DB::beginTransaction();
+        try {
+            while (!gzeof($gzStream) && $count < $limit) {
+                $buffer .= gzread($gzStream, 4096);
+                while (($pos = strpos($buffer, "\n")) !== false && $count < $limit) {
+                    $line = substr($buffer, 0, $pos);
+                    $buffer = substr($buffer, $pos + 1);
+
+                    $product = json_decode($line, true);
+                    if (is_array($product)) {
+                        $product['imported_t'] = now();
+                        Product::create($product);
+                        $count++;
+                    }
+                }
+
             }
-            gzclose($gzFile);
-            fclose($jsonFile);
-            unlink($gzFilePath);
-            $this->info("Importing the JSON file into the database...");
-            $this->importFileToDatabase($jsonFilePath);
-            $this->markFileAsImported($file);
+            DB::commit();
+            $this->info("Imported $count items from {$file}.");
+        } catch (\Exception $e) {
+            DB::rollBack();
+            $this->error("Error importing file {$file}: " . $e->getMessage());
+        } finally {
+            gzclose($gzStream);
         }
-
     }
 
     /**
@@ -91,70 +88,23 @@ class CheckDataFiles extends Command
      */
     protected function checkIfFileIsImported(string $file): bool
     {
-        if (DB::table('data_files')->where('name', $file)->whereDate('last_checked','=', today())->exists()) {
-            $this->info("File {$file} already imported. Skipping...");
-            return true;
-        }
-        return false;
+        $exists = DB::table('data_files')
+            ->where('name', $file)
+            ->exists();
+
+        if ($exists) $this->info("File {$file} already imported. Skipping...");
+        return $exists;
     }
 
     /**
+     * @param string $file
      * @return void
      */
-    protected function verifyFolders(): void
+    protected function markFileAsImported(string $file): void
     {
-        if (!Storage::directoryExists('compressed')) {
-            Storage::makeDirectory('compressed');
-        }
-        if (!Storage::directoryExists('json')) {
-            Storage::makeDirectory('json');
-        }
-    }
-
-    /**
-     * @return void
-     */
-    protected function cleanDirectories(): void
-    {
-        $this->info("Cleaning directories...");-
-        Storage::deleteDirectory('compressed');
-        Storage::deleteDirectory('json');
-    }
-
-    /**
-     * @param $jsonFilePath
-     * @return void
-     */
-    protected function importFileToDatabase($jsonFilePath): void
-    {
-        $jsonFile = fopen($jsonFilePath, 'r');
-        $count = 1;
-        while (!feof($jsonFile)) {
-            $line = fgets($jsonFile);
-            $product = json_decode($line, true);
-            if ($product) {
-                $product['imported_t'] = now();
-                Product::query()->create($product);
-            }
-            if ($count >= $this->option('limit')) {
-                break;
-            }
-            $count++;
-
-        }
-        fclose($jsonFile);
-        unlink($jsonFilePath);
-        $this->info('File downloaded, extracted, and saved successfully with streams!');
-    }
-
-    protected function markFileAsImported(string $file)
-    {
-        DB::table('data_files')->insert(
-            [
-                'name' => $file,
-                'last_checked' => now()
-            ],
-
-        );
+        DB::table('data_files')->insert([
+            'name' => $file,
+            'last_checked' => now()
+        ]);
     }
 }
